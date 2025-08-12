727c25784f0ca6e494eb23518c5b719a53002a83
preston-horne <209566610+preston-horne@users.noreply.github.com>
2025-08-12 12:43:38 -0500

    made text smaller for iphone compatibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
 
   return (
     <div className="h-96 flex items-center justify-center">
-      <div className="text-5xl sm:text-6xl md:text-7xl font-mono bg-black text-green-300 p-6 sm:p-8 md:p-12 lg:p-16 rounded-xl inline-flex items-center justify-center">
+      <div className="text-3xl sm:text-4xl md:text-5xl font-mono bg-black text-green-300 p-6 sm:p-8 md:p-12 lg:p-16 rounded-xl inline-flex items-center justify-center">
         <span className="flex items-center">
           {displayText}
           <span className={`inline-block w-2 sm:w-3 md:w-4 h-12 sm:h-16 md:h-20 lg:h-24 xl:h-28 bg-green-400 ml-2 ${showCursor ? 'opacity-100' : 'opacity-0'} transition-opacity duration-100`} />
